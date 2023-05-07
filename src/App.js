322c7b639033c35dc67112cda21a1a0261578932
AllahvelS <allahvelsalisbury@pursuit.org>
2023-05-07 02:39:53 -0400

    Travel Blog edited.

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ function App() {
     <div className="bg-warning p-3 d-flex flex-row justify-content-center">
         <ul className="list-unstyled mr-5 m-0">
             <li id="footer-icons"> Contact Me </li>
-            <li><a className="text-decoration-none text-dark" href=""> About The Author </a></li>
+            <li><a className="text-decoration-none text-dark" href="https://github.com/AllahvelS"> About The Author </a></li>
             <li> Copyright <strong>MyTravelBlog</strong> 2020 </li>
         </ul>
     </div>
